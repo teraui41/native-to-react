0b82000b66ab08adbcb3757d379c7f172b9950e8
Edmond Wu <edmond.wu@velocitylight.net>
2021-01-08 18:13:52 +0800

    Init ios project

---
+++ ios/reactApp/reactApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  reactApp
+//
+//  Created by Edmond Wu on 2021/1/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
