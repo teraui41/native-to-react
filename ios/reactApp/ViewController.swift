57706107a2a6d9be060b4b24f4329c6dd8da29dd
Edmond Wu <edmondwu@EdmondMac.local>
2021-01-10 10:15:31 +0800

    add ios native

--- ios/reactApp/ViewController.swift
+++ ios/reactApp/ViewController.swift
@@ -6,12 +6,35 @@
 //
 
 import UIKit
+import React
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+       
+    }
+    
+    @IBAction func highScoreButtonTapped(sender : UIButton) {
+        let jsCodeLocation:URL! = URL(string: "http://localhost:8081/index.bundle?platform=ios")
+        let mockData:NSDictionary = ["scores":
+            [
+                ["name":"Alex", "value":"42"],
+                ["name":"Joel", "value":"10"]
+            ]
+        ]
+
+        let rootView = RCTRootView(
+            bundleURL: jsCodeLocation,
+            moduleName: "RNMyReactApp",
+            initialProperties: mockData as [NSObject : AnyObject],
+            launchOptions: nil
+        )
+        let vc = UIViewController()
+        vc.view = rootView
+        self.present(vc, animated: true, completion: nil)
     }
 
 
